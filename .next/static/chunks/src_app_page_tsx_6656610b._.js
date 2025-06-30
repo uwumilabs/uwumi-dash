5082a5c767243a3fa343bd0c0e044a3f209cc623
Durgesh Kumar Dwivedi <durgeshdwivedi81@gmail.com>
2025-06-30 21:05:39 +0530

    Remove .next and next-env.d.ts from tracking and update .gitignore

--- .next/static/chunks/src_app_page_tsx_6656610b._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_a91ec960._.js",
-  "static/chunks/node_modules_3d8759d6._.js"
-],
-    source: "dynamic"
-});
